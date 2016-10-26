e140d2d6171606062149bc6604c4cacbcc8abb27
derekviera <ma.dmviera01@gmail.com>
2016-10-26 13:25:26 -0400

    remove base64

--- b-is-4-blog.rb
+++ b-is-4-blog.rb
@@ -4,7 +4,7 @@ require "cuba/render"
 require 'erb'
 require 'sequel'
 require 'date'
-require 'base64'
+
 
 Cuba.use Rack::Session::Cookie, :secret => "wVmZ0Xk+AUsBTA55YIjLLzWNUlk/jnTnU7orZ5vAAeQqVi0iOe3pex/7jtIKh9+RaJ/IBwBubKjna5zQ2tLkKA=="
 Cuba.use Rack::Protection
@@ -20,16 +20,16 @@ DB = Sequel.connect('sqlite://db/blog.db', :max_connections=>200)
   Cuba.use Rack::Static, root: "public", urls: ["/css"]
   Cuba.use Rack::Static, root: "public", urls: ["/imgs"]
   Cuba.use Rack::Static, root: "public", urls: ["/js"]
-  
+
   Cuba.plugin(Cuba::Render)
 
   Cuba.settings[:render][:template_engine] = "erb"
   Cuba.settings[:render][:views] = "./views"
- 
+
 Cuba.define do
   on get do
-  
-    on root do 
+
+    on root do
       @css = "<link rel='stylesheet' type='text/css' href='/css/style.css'>"
       where_post =  posts.where[:title => 'Lorem ipsum dolor sit amet']
       @title = where_post.fetch(:title)
@@ -37,14 +37,14 @@ Cuba.define do
       @footer = "&copy Derek Viera"
       res.write view("layout")
     end
-    
+
   on "articles" do
-	where_post =  posts.where[:id => 2]
-	@title = where_post.fetch(:title)  
+	  where_post =  posts.where[:id => 2]
+	  @title = where_post.fetch(:title)
     @css = "<link rel='stylesheet' type='text/css' href='/css/style.css'>"
-	res.write partial ("articles")
+	  res.write partial ("articles")
 	end
-	
+
 	on "search", param("search") do |query|
 	  @css = "<link rel='stylesheet' type='text/css' href='/css/style.css'>"
 	  where_post = posts.where[:id => "#{query}"]
@@ -52,4 +52,4 @@ Cuba.define do
       res.write partial ("articles")
     end
     end
-end  
+end
